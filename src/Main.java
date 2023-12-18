00d96205ac7ac464e8fcd14b120cb129f9ebaeb2
Daniil <D.aniil_2005>
2023-12-18 11:50:32 +0600

    zhannur

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import models.Point;
+import models.Shape;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File(/Users/macbook/IdeaProjects/assighment 1/src/models/source);
+        Scanner sc = new Scanner(file);
+        Shape shape = new Shape();
+        while (sc.hasNextDouble()){
+            double x = sc.nextDouble();
+            double y = sc.next.Doule();
+        }
+    }
+}
---
+++ src/models/Point.java
@@ -0,0 +1,6 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+}
